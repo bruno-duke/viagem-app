84e5d516435308e3a0dc1713697583872d5dec95
Bruno Leite <brunoleite@Brunos-MacBook-Pro.local>
2022-09-28 06:18:48 -0300

    findById

--- agencia-app/src/main/java/org/brunofsleite/cliente/ClienteResource.java
+++ agencia-app/src/main/java/org/brunofsleite/cliente/ClienteResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/cliente")
@@ -14,5 +15,12 @@ public class ClienteResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Cliente> get() {
         return Cliente.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente findById(@QueryParam("id") long id) {
+        return Cliente.findById(id);
     }
 }
