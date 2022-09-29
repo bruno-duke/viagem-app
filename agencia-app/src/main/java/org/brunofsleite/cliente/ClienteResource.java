08a2dee97d754de24905217225c4069e7aae04eb
Bruno Leite <brunoleite@Brunos-MacBook-Pro.local>
2022-09-29 07:28:48 -0300

    criado o deleteById e newCliente

--- agencia-app/src/main/java/org/brunofsleite/cliente/ClienteResource.java
+++ agencia-app/src/main/java/org/brunofsleite/cliente/ClienteResource.java
@@ -2,11 +2,10 @@ package org.brunofsleite.cliente;
 
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/cliente")
 public class ClienteResource {
@@ -22,5 +21,23 @@ public class ClienteResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Cliente findById(@QueryParam("id") long id) {
         return Cliente.findById(id);
+    }
+
+    @Transactional
+    @DELETE
+    @Path("deleteById")
+    public void deleteById(@QueryParam("id") long id) {
+        Cliente.deleteById(id);
+    }
+
+    @Transactional
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newCliente(Cliente cliente) {
+        cliente.id = null;
+        cliente.persist();
+
+        return Response.status(Response.Status.CREATED).entity(cliente).build();
     }
 }
